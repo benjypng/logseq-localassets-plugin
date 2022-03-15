eb4f98fa4f73684dc21cb316b97e21ee0b011a71
hkgnp <ben@pngs.cc>
2022-03-15 18:25:37 +0800

    streamlined workflow to autodetect file type

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import "@logseq/libs";
+import { renderEmbedAsset } from "./renderEmbedAsset";
+
+const id = Math.random()
+    .toString(36)
+    .replace(/[^a-z]+/g, "");
+
+const main = () => {
+    logseq.Editor.registerSlashCommand("Embed local file", async () => {
+        const currBlk = await logseq.Editor.getCurrentBlock();
+        const uuid = currBlk.uuid;
+
+        const fileInput = document.createElement("input");
+        fileInput.type = "file";
+        fileInput.onchange = async (e) => {
+            const { type, path } = (<HTMLInputElement>e.target).files[0];
+
+            if (
+                type === "application/pdf" ||
+                type === "image/png" ||
+                type === "image/tiff" ||
+                type === "image/bmp" ||
+                type === "image/gif" ||
+                type === "image/jpeg"
+            ) {
+                await logseq.Editor.updateBlock(uuid, `![${id}](${path})`);
+            } else if (type === "audio/mpeg") {
+                await logseq.Editor.updateBlock(
+                    uuid,
+                    `[:audio {:controls true :src "${path}"}]`
+                );
+            } else if (type === "video/mpeg" || type === "video/mp4") {
+                await logseq.Editor.updateBlock(
+                    uuid,
+                    `[:video {:controls true :src "${path}"}]`
+                );
+            } else {
+                await logseq.Editor.updateBlock(uuid, `[📄 ${id}](${path})`);
+            }
+            await logseq.Editor.exitEditingMode();
+        };
+        fileInput.click();
+    });
+};
+
+logseq.ready(main).catch(console.error);
