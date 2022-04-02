2dcf95d231781ad97df669f2d8491340dc5e2a78
hkgnp <ben@pngs.cc>
2022-04-02 16:47:35 +0800

    Looks like need to whitelist

--- src/index.ts
+++ src/index.ts
@@ -11,58 +11,44 @@ interface Files {
 
 const main = () => {
   console.log("logseq-localassets-plugin loaded");
+  logseq.Editor.registerSlashCommand("Embed local file", async () => {
+    const currBlk = await logseq.Editor.getCurrentBlock();
+    const uuid = currBlk.uuid;
 
-  const handleFile = async (e, uuid) => {
-    const { type, path }: Files = (<HTMLInputElement>e.target).files[0];
+    const fileInput = document.createElement("input");
+    const btn = document.createElement("button");
+    fileInput.type = "file";
+    fileInput.onchange = async (e) => {
+      const { type, path }: Files = (<HTMLInputElement>e.target).files[0];
 
-    if (
-      type === "application/pdf" ||
-      type === "image/png" ||
-      type === "image/tiff" ||
-      type === "image/bmp" ||
-      type === "image/gif" ||
-      type === "image/jpeg"
-    ) {
-      await logseq.Editor.updateBlock(uuid, `![${id}](${path})`);
-    } else if (type === "audio/mpeg") {
-      await logseq.Editor.updateBlock(
-        uuid,
-        `[:audio {:controls true :src "${path}"}]`
-      );
-    } else if (type === "video/mpeg" || type === "video/mp4") {
-      await logseq.Editor.updateBlock(
-        uuid,
-        `[:video {:controls true :src "${path}"}]`
-      );
-    } else {
-      await logseq.Editor.updateBlock(uuid, `[📄 ${id}](${path})`);
-    }
-    await logseq.Editor.exitEditingMode();
-  };
-
-  const fileInput = document.createElement("input");
-  fileInput.type = "file";
-  fileInput.id = "theFile";
-  fileInput.style.display = "none";
-  document.body.appendChild(fileInput);
-  logseq.provideModel({
-    async embedLocalAsset() {
-      //const currBlk = await logseq.Editor.getCurrentBlock();
-      // const uuid = currBlk.uuid;
-      //     fileInput.onchange = async (e) => {};
-      var elem = document.getElementById("theFile");
-      if (elem && document.createEvent) {
-        var evt = document.createEvent("MouseEvents");
-        evt.initEvent("click", true, false);
-        elem.dispatchEvent(evt);
+      if (
+        type === "application/pdf" ||
+        type === "image/png" ||
+        type === "image/tiff" ||
+        type === "image/bmp" ||
+        type === "image/gif" ||
+        type === "image/jpeg"
+      ) {
+        await logseq.Editor.updateBlock(uuid, `![${id}](${path})`);
+      } else if (type === "audio/mpeg") {
+        await logseq.Editor.updateBlock(
+          uuid,
+          `[:audio {:controls true :src "${path}"}]`
+        );
+      } else if (type === "video/mpeg" || type === "video/mp4") {
+        await logseq.Editor.updateBlock(
+          uuid,
+          `[:video {:controls true :src "${path}"}]`
+        );
+      } else {
+        await logseq.Editor.updateBlock(uuid, `[📄 ${id}](${path})`);
       }
-    },
-  });
-
-  // Register UI
-  logseq.App.registerUIItem("toolbar", {
-    key: "logseq-localassets-plugin",
-    template: `<label for="theFile2"><i class="ti ti-file-symlink"></i></label><input id="theFile2" type="file" hidden />`,
+      await logseq.Editor.exitEditingMode();
+    };
+    btn.addEventListener("click", () => {
+      fileInput.click();
+    });
+    btn.click();
   });
 };
 
