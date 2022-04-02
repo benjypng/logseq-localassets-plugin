a9a0413ab981ab34c7fcf980edaa6b906b6db015
hkgnp <ben@pngs.cc>
2022-04-02 14:48:58 +0800

    trying to fix permissions

--- src/index.ts
+++ src/index.ts
@@ -10,39 +10,46 @@ interface Files {
 }
 
 const main = () => {
+  const handleFile = async (e, uuid) => {
+    const { type, path }: Files = (<HTMLInputElement>e.target).files[0];
+
+    if (
+      type === "application/pdf" ||
+      type === "image/png" ||
+      type === "image/tiff" ||
+      type === "image/bmp" ||
+      type === "image/gif" ||
+      type === "image/jpeg"
+    ) {
+      await logseq.Editor.updateBlock(uuid, `![${id}](${path})`);
+    } else if (type === "audio/mpeg") {
+      await logseq.Editor.updateBlock(
+        uuid,
+        `[:audio {:controls true :src "${path}"}]`
+      );
+    } else if (type === "video/mpeg" || type === "video/mp4") {
+      await logseq.Editor.updateBlock(
+        uuid,
+        `[:video {:controls true :src "${path}"}]`
+      );
+    } else {
+      await logseq.Editor.updateBlock(uuid, `[📄 ${id}](${path})`);
+    }
+    await logseq.Editor.exitEditingMode();
+  };
+
+  const fileInput = document.createElement("input");
+  fileInput.type = "file";
+  fileInput.id = "theFile";
+  fileInput.style.display = "none";
+  console.log(fileInput);
+
   logseq.provideModel({
     async embedLocalAsset() {
-      const currBlk = await logseq.Editor.getCurrentBlock();
-      const uuid = currBlk.uuid;
-      const fileInput = document.createElement("input");
-      fileInput.type = "file";
-      fileInput.onchange = async (e) => {
-        const { type, path }: Files = (<HTMLInputElement>e.target).files[0];
-
-        if (
-          type === "application/pdf" ||
-          type === "image/png" ||
-          type === "image/tiff" ||
-          type === "image/bmp" ||
-          type === "image/gif" ||
-          type === "image/jpeg"
-        ) {
-          await logseq.Editor.updateBlock(uuid, `![${id}](${path})`);
-        } else if (type === "audio/mpeg") {
-          await logseq.Editor.updateBlock(
-            uuid,
-            `[:audio {:controls true :src "${path}"}]`
-          );
-        } else if (type === "video/mpeg" || type === "video/mp4") {
-          await logseq.Editor.updateBlock(
-            uuid,
-            `[:video {:controls true :src "${path}"}]`
-          );
-        } else {
-          await logseq.Editor.updateBlock(uuid, `[📄 ${id}](${path})`);
-        }
-        await logseq.Editor.exitEditingMode();
-      };
+      //const currBlk = await logseq.Editor.getCurrentBlock();
+      // const uuid = currBlk.uuid;
+      //     fileInput.onchange = async (e) => {};
+      //@ts-ignore
       fileInput.click();
     },
   });
