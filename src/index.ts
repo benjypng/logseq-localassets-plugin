6efa73609379db32154e3f7b4e1d6e17bc720b00
hkgnp <ben@pngs.cc>
2022-04-02 13:53:53 +0800

    Fixing permissions issue

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,61 @@
 import "@logseq/libs";
-import { renderEmbedAsset } from "./renderEmbedAsset";
 
 const id = Math.random()
   .toString(36)
   .replace(/[^a-z]+/g, "");
 
+interface Files {
+  type: string;
+  path?: string;
+}
+
 const main = () => {
-  logseq.Editor.registerSlashCommand("Embed local file", async () => {
-    const currBlk = await logseq.Editor.getCurrentBlock();
-    const uuid = currBlk.uuid;
+  logseq.provideModel({
+    async embedLocalAsset() {
+      const currBlk = await logseq.Editor.getCurrentBlock();
+      const uuid = currBlk.uuid;
+      const fileInput = document.createElement("input");
+      fileInput.type = "file";
+      fileInput.onchange = async (e) => {
+        const { type, path }: Files = (<HTMLInputElement>e.target).files[0];
 
-    const fileInput = document.createElement("input");
-    const btn = document.createElement("button");
-    fileInput.type = "file";
-    fileInput.onchange = async (e) => {
-      const { type, path } = (<HTMLInputElement>e.target).files[0];
-
-      if (
-        type === "application/pdf" ||
-        type === "image/png" ||
-        type === "image/tiff" ||
-        type === "image/bmp" ||
-        type === "image/gif" ||
-        type === "image/jpeg"
-      ) {
-        await logseq.Editor.updateBlock(uuid, `![${id}](${path})`);
-      } else if (type === "audio/mpeg") {
-        await logseq.Editor.updateBlock(
-          uuid,
-          `[:audio {:controls true :src "${path}"}]`
-        );
-      } else if (type === "video/mpeg" || type === "video/mp4") {
-        await logseq.Editor.updateBlock(
-          uuid,
-          `[:video {:controls true :src "${path}"}]`
-        );
-      } else {
-        await logseq.Editor.updateBlock(uuid, `[📄 ${id}](${path})`);
-      }
-      await logseq.Editor.exitEditingMode();
-    };
-    btn.addEventListener("click", () => {
+        if (
+          type === "application/pdf" ||
+          type === "image/png" ||
+          type === "image/tiff" ||
+          type === "image/bmp" ||
+          type === "image/gif" ||
+          type === "image/jpeg"
+        ) {
+          await logseq.Editor.updateBlock(uuid, `![${id}](${path})`);
+        } else if (type === "audio/mpeg") {
+          await logseq.Editor.updateBlock(
+            uuid,
+            `[:audio {:controls true :src "${path}"}]`
+          );
+        } else if (type === "video/mpeg" || type === "video/mp4") {
+          await logseq.Editor.updateBlock(
+            uuid,
+            `[:video {:controls true :src "${path}"}]`
+          );
+        } else {
+          await logseq.Editor.updateBlock(uuid, `[📄 ${id}](${path})`);
+        }
+        await logseq.Editor.exitEditingMode();
+      };
       fileInput.click();
-    });
-    btn.click();
+    },
+  });
+
+  // Register UI
+  logseq.App.registerUIItem("toolbar", {
+    key: "logseq-localassets-plugin",
+    template: `
+      <a data-on-click="embedLocalAsset"
+        class="button">
+        <i class="ti ti-file-symlink"></i>
+       </a>
+`,
   });
 };
 
