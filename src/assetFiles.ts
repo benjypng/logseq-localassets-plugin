95c4457c75a785d1428edb5daa28dae780193006
hkgnp <ben@pngs.cc>
2022-05-05 22:35:43 +0800

    Added support for mobile linking

---
+++ src/assetFiles.ts
@@ -0,0 +1,50 @@
+interface Files {
+  name: string;
+  type: string;
+  path?: string;
+}
+
+export const assetFiles = () => {
+  logseq.Editor.registerSlashCommand("Embed asset file", async () => {
+    const currBlk = await logseq.Editor.getCurrentBlock();
+    const uuid = currBlk.uuid;
+
+    const fileInput = document.createElement("input");
+    const btn = document.createElement("button");
+    fileInput.type = "file";
+    fileInput.onchange = async (e) => {
+      const { type, name }: Files = (<HTMLInputElement>e.target).files[0];
+
+      if (
+        type === "application/pdf" ||
+        type === "image/png" ||
+        type === "image/tiff" ||
+        type === "image/bmp" ||
+        type === "image/gif" ||
+        type === "image/jpeg"
+      ) {
+        await logseq.Editor.updateBlock(uuid, `![${name}](../assets/${name})`);
+      } else if (type === "audio/mpeg") {
+        await logseq.Editor.updateBlock(
+          uuid,
+          `[:audio {:controls true :src "../assets/${name}"}]`
+        );
+      } else if (type === "video/mpeg" || type === "video/mp4") {
+        await logseq.Editor.updateBlock(
+          uuid,
+          `[:video {:controls true :src "../assets/${name}"}]`
+        );
+      } else {
+        await logseq.Editor.updateBlock(
+          uuid,
+          `[📄 ${name}](../assets/${name})`
+        );
+      }
+      await logseq.Editor.exitEditingMode();
+    };
+    btn.addEventListener("click", () => {
+      fileInput.click();
+    });
+    btn.click();
+  });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,13 @@
 import "@logseq/libs";
-
-const id = Math.random()
-  .toString(36)
-  .replace(/[^a-z]+/g, "");
-
-interface Files {
-  type: string;
-  path?: string;
-}
+import { assetFiles } from "./assetFiles";
+import { nonAssetFiles } from "./nonAssetFiles";
 
 const main = async () => {
   console.log("logseq-localassets-plugin loaded");
 
-  logseq.Editor.registerSlashCommand("Embed local file", async () => {
-    const currBlk = await logseq.Editor.getCurrentBlock();
-    const uuid = currBlk.uuid;
+  nonAssetFiles();
 
-    const fileInput = document.createElement("input");
-    const btn = document.createElement("button");
-    fileInput.type = "file";
-    fileInput.onchange = async (e) => {
-      const { type, path }: Files = (<HTMLInputElement>e.target).files[0];
-
-      if (
-        type === "application/pdf" ||
-        type === "image/png" ||
-        type === "image/tiff" ||
-        type === "image/bmp" ||
-        type === "image/gif" ||
-        type === "image/jpeg"
-      ) {
-        await logseq.Editor.updateBlock(uuid, `![${id}](${path})`);
-      } else if (type === "audio/mpeg") {
-        await logseq.Editor.updateBlock(
-          uuid,
-          `[:audio {:controls true :src "${path}"}]`
-        );
-      } else if (type === "video/mpeg" || type === "video/mp4") {
-        await logseq.Editor.updateBlock(
-          uuid,
-          `[:video {:controls true :src "${path}"}]`
-        );
-      } else {
-        await logseq.Editor.updateBlock(uuid, `[📄 ${id}](${path})`);
-      }
-      await logseq.Editor.exitEditingMode();
-    };
-    btn.addEventListener("click", () => {
-      fileInput.click();
-    });
-    btn.click();
-  });
+  assetFiles();
 
   ///////// OLD /////////
   const currGraph = await logseq.App.getCurrentGraph();
